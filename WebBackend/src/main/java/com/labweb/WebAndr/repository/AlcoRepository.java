2283378bad12bec38c6afdaa2163e904d8a404f1
FoXm1 <119307136+F0Xm1@users.noreply.github.com>
2023-12-15 20:02:23 +0200

    Delete WebBackend directory

--- WebBackend/src/main/java/com/labweb/WebAndr/repository/AlcoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.labweb.WebAndr.repository;
-
-import com.labweb.WebAndr.entity.AlcoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlcoRepository extends JpaRepository<AlcoEntity, Long> {
-}
